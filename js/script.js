5e0fd1fe4dea130c0945ee8c3ee3736e165dfc33
Omar Qureshi <omarqureshi687@gmail.com>
2019-11-13 17:54:04 -0500

    added posenet

--- js/script.js
+++ js/script.js
@@ -1,6 +1,48 @@
-function setup(){
+let video;
+let poseNet;
+let nose = [];
+let img;
+
+function preload() {
+  img = loadImage('assets/happypotato.png');
+}
+
+function setup() {
+  createCanvas(640, 480);
+  preload();
+  //turns on webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  //initialize poseNet
+  poseNet = ml5.poseNet(video, modelLoaded);
+
+  //look for new poses
+  poseNet.on("pose", function(results) {
+    if(results){
+      for(var i = 0; i < results.length; i++){
+        nose[i] = results[0].pose.nose;
+      };
+    };
+  });
 }
 
 function draw() {
+  //draws the video input onto the canvas
+  image(video, 0, 0);
+  
+  //draws circles on everyones nose
+  if(nose){
+    // console.log(nose.length);
+    for(var i = 0; i < nose.length; i++){
+      // ellipse(nose[i].x, nose[i].y, 50);
+      image(img, nose[i].x-150, nose[i].y-190, 370, 350);
+    };
+  };
+}
 
+function modelLoaded() {
+  console.log("poseNet has loaded");
+
+  poseNet.multiPose(video);
 }
